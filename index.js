8a1bb2e79bccbf920675c53cc70d32677d19dcb2
TOURE SOULEYMANE <toure5013@gmail.com>
2020-09-02 13:31:22 +0000

    Change to fonctions

--- index.js
+++ index.js
@@ -7,44 +7,65 @@ var request = require('request');
 | OBJECT_TYPE : image, video, audio, file
 | PAGE_ACCESS_TOKEN : token generated on https://developers.facebook.com/docs/marketing-api/access/ that give  
 | access to facebook graph api
+|
+|@return
+|body: '{"attachment_id":"610873522933283"}'
 */
-var OBJECT_URL = "https://proxymedia.woopic.com/api/v1/images/1202%2F/offre-options/seo-modalites-de-resiliation.jpg"
-var OBJECT_TYPE = 'image'
-const PAGE_ACCESS_TOKEN = "EAAGU9tyMafUBAJ4BCwVis0Svhzh56ZBZBYBWgexTmhj61V4nmuIkxCgtHStWpN2uzeeM6jO2yUQADOzmZBZBzKOsZCHZBmfDwKw9L7HJYwPBEPvKTs81B2XZCNgRgZAr0i61wHdCWpkk8pkyYZAAYAgBLqZBlZBo5JPDJTF3GRY2Qp0nl5CMfPg5A58UG5LxYlbZCvgZD";
 
 
-var headers = {
-  'Content-Type': 'application/json'
-};
+var imageUploadWithFbGrphApi = (OBJECT_URL_PARAM, OBJECT_TYPE_PARAM) => {
+  var OBJECT_URL = OBJECT_URL_PARAM //"https://proxymedia.woopic.com/api/v1/images/1202%2F/offre-options/seo-modalites-de-resiliation.jpg"
+  var OBJECT_TYPE = OBJECT_TYPE_PARAM || "image" ; //image
+  const PAGE_ACCESS_TOKEN = "EAAGU9tyMafUBACq0hqsNANtwMbu5EsQiyvtCynZBuURLoYEKaEzL5MgIn0IoojAEUeCG4WmZByc6tqQvFKcToXDcesMJZAkUIMcvqLY1ar5cqv39Vnt0gZC1hqRZBYnoADt5yg5uBUcZBLhXF2oHi06ldwDxV2pWanR6TgyLjAZCyImFHZCox3zkvmhe6i6OCNhKaK6cFnlOnQZDZD";
+  var headers = {
+    'Content-Type': 'application/json'
+  };
 
-var dataString = '{ "message":{ "attachment":{ "type":"' + OBJECT_TYPE + '", "payload":{ "is_reusable": true, "url":"' + OBJECT_URL + '" } } } }';
+  var dataString = '{ "message":{ "attachment":{ "type":"' + OBJECT_TYPE + '", "payload":{ "is_reusable": true, "url":"' + OBJECT_URL + '" } } } }';
 
-var URL = 'https://graph.facebook.com/v2.6/106650577838707/message_attachments?access_token='+PAGE_ACCESS_TOKEN
-var options = {
-  url: URL,
-  method: 'POST',
-  headers: headers,
-  body: dataString
-};
+  var URL = 'https://graph.facebook.com/v2.6/106650577838707/message_attachments?access_token=' + PAGE_ACCESS_TOKEN
+  var options = {
+    url: URL,
+    method: 'POST',
+    headers: headers,
+    body: dataString
+  };
 
-function callback(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log('-----------------AN ERROR OCCURED -----------------')
-    console.log("ERROR : " + JSON.stringify(error));
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log('-----------------AN ERROR OCCURED -----------------')
+      console.log("ERROR : " + JSON.stringify(error));
+    }
+    console.log("THE OBJECT IN BETTER UPLOADED");
+    console.log(response.body)
+    return response.body;
   }
-  console.log("THE OBJECT IN BETTER UPLOADED");
-  console.log(response)
+  request(options, callback);
 }
 
-request(options, callback);
+var imgurl = "https://smartly-image-stage.s3.us-west-2.amazonaws.com/1596535438194.png"
+//imageUploadWithFbGrphApi(imgurl, "image")
 
 
 /*
-  //Smartly side
-  { 
-    "type": "video",
+|-----|Smartly side|-----|
+|@params
+|OBJECT_TYPE : image, video, audio, file
+|Use the api return
+|@return
+|body: '{"attachment_id":"ATACHEMENT_ID_RETURNED"}'
+  {
+    "type": "OBJECT_TYPE",
     "payload": {
-      "attachment_id": "123452640641789" / * au lieu de« url »:« https://blabla.com »* /
+      "attachment_id": "ATACHEMENT_ID_RETURNED" / * au lieu de« url »:« https://blabla.com »* /
     }
   }
- */
+*/
+ /*
+ {
+    "type": "image",
+    "payload": {
+      "attachment_id": "349322022869623" 
+    }
+  }
+*/
